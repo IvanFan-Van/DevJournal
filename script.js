321f5875b0bbd746d786963f7bf5cf6e307c7427
IvanFan-Van <98149954+IvanFan-Van@users.noreply.github.com>
2025-06-29 22:15:10 +0800

    feat: add loading page (#6)

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ import Cat from "./scripts/cat.js";
 
 const catContainer = document.getElementById("cat-container");
 const cat = new Cat(catContainer);
+
+// PRELOADING
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", function () {
+    loadingElement.classList.add("loaded");
+    document.body.classList.remove("active");
+});
